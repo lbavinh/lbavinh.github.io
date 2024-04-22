91e425442b154e62df8c09741dbd0cd7b255a5f4
Han Nguyen <nhbhan34@gmail.com>
2024-04-22 09:11:12 +0300

    Add dark mode

---
+++ assets/light_dark_mode/Creating-a-Dark-Mode-Switcher-with-CSS-and-JavaScript-main/Course files/js/app.js
@@ -0,0 +1,18 @@
+// Switch function
+const switchTheme = () => {
+// Get root element and data-theme value
+const rootElem = document.documentElement
+let dataTheme = rootElem.getAttribute('data-theme'),
+    newTheme
+
+newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+// set the new HTML attribute
+rootElem.setAttribute('data-theme', newTheme)
+
+// Set the new local storage item
+localStorage.setItem('theme', newTheme)
+}
+
+// Add event listener for the theme switcher
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
